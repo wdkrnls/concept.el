144f5797e5ec5dbe637c7f2506f48dae8d72ec2b
Kyle Andrews <kyle.c.andrews@gmail.com>
2026-09-17 18:20:58 -0700

    This is my best guess for the cause of the concept-map--after-changes issues

--- concept.el
+++ concept.el
@@ -7351,10 +7351,11 @@ If it doesn't parse, move the point to where the first failure is."
 (defun concept-map-grammar-parses-p ()
   "Return t if the current concept map parses, otherwise return nil.
 On failure, leave point at the first parse error."
-  (condition-case nil
-      (concept-map-check-parse)
-    (user-error
-     nil)))
+  (save-excursion
+    (condition-case nil
+        (concept-map-check-parse)
+      (user-error
+       nil))))
 
 (defvar-local concept-map-network-is-stale
     nil
